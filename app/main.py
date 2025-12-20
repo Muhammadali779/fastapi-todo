f7db2d1b8c6b75ffda5b43eeacbb65d797b70392
djumanov <djumanovdev@gmail.com>
2025-12-20 09:12:28 +0500

    formatted code

--- app/main.py
+++ app/main.py
@@ -1,18 +1,7 @@
 from fastapi import FastAPI
 
-from app.core.database import engine, Base
-from app.models import user, task
 from app.api.router import router
 
-app = FastAPI(
-    title='Todo List',
-    version='1.0.0',
-    description='FastAPI Todo List API'
-)
+app = FastAPI(title="Todo List", version="1.0.0", description="FastAPI Todo List API")
 
-Base.metadata.create_all(engine)
-
-app.include_router(
-    router,
-    prefix='/api',
-)
+app.include_router(router, prefix="/api")
